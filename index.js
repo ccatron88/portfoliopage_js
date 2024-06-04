867005e9afc7f1a1e72b7edec439370303dc1268
ccatron88 <christophercatron@outlook.com>
2024-06-03 22:34:52 -0500

    Remove old Twitter logo and link Removed html for old Twitter logo and link.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let aboutMeEl = document.querySelector(".aboutme_container")
 
 document.addEventListener("click", e => {
     if (e.target.classList.contains('arrow')) {
-        console.log('Test Passed')
         window.location = '#aboutme_container'
         history.replaceState(null, null, ' ')
     }
